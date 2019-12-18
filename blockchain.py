59d4e2e83703fdbf7c4137b2342e5ce2add47a4b
Hayden Key <hwkey.77@gmail.com>
2019-12-18 00:36:58 -0500

    added block representation

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,65 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transactions(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_block(self, proof, previous_hash):
+        """
+        Creates a new Block and adds it into the Blockchain
+        :param proof: <int> The proof given by the Proof of Work Algorithm
+        :param previous_hash: (optional) <str> Hash of Previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to add to the next block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
 
-    @staticmethod
-    def last_block(self):
-        # Returns last block in the chain
-        pass
+        # Must make sure the Dictionary is ordered or we will have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
